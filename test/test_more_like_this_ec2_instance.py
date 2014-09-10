e27706b7bb3c686f8065c562962d7d1213e5add5
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-09-10 17:48:46 +0900

    Create base test module

--- test/test_more_like_this_ec2_instance.py
+++ test/test_more_like_this_ec2_instance.py
@@ -1,43 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import boto.ec2
 import boto.ec2.instance
 import boto.ec2.image
 import moto
 import nose.tools
 
-import more_like_this
+import test.base
 
 
-class TestMoreLikeThisEC2InstanceWithoutEBS(object):
-
-    @moto.mock_ec2
-    def create_instance(self):
-        self.conn.run_instances(
-            image_id='ami-HOGEHOGE'
-        )
-        reservations = self.conn.get_all_instances()
-        return reservations[0].instances[0]
-
-    @moto.mock_ec2
-    def setup(self):
-        self.conn = boto.ec2.connect_to_region(
-            region_name='us-east-1',
-            aws_access_key_id='HOGEHOGE',
-            aws_secret_access_key='FUGAFUGA'
-        )
-        instance = self.create_instance()
-        self.conn.create_image(
-            instance_id=instance.id,
-            name='TESTING_IMAGE'
-        )
-        self.image = self.conn.get_all_images()[0]
-        self.more_like_this = more_like_this.MoreLikeThisEC2Instance()
-
-    @moto.mock_ec2
-    def teardown(self):
-        self.conn.close()
+class TestMoreLikeThisEC2InstanceWithoutEBS(
+    test.base.BaseMoreLikeThisEC2Instance
+):
 
     @moto.mock_ec2
     def test_set_base_image(self):
